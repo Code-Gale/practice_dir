23c1aa5d53210ea53ca465fd19390088ba9f449c
Code-Gale <codewhiz001@gmail.com>
2024-04-03 14:01:47 -0700

    calculator app using module and require concepts

---
+++ node/exercises/calculator/app.js
@@ -0,0 +1,9 @@
+const { add, mul, sub, div} = require('./calculator.js')
+
+const a = 5;
+const b = 10
+
+console.log("Addition :", add(a, b))
+console.log("Subraction :", sub(a, b))
+console.log("Multiplication :", mul(a, b))
+console.log("Division :", div(a, b))
