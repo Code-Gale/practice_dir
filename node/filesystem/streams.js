60183dfda8e7834c7a7f6d386d05dbbef5555e12
Code-Gale <codewhiz001@gmail.com>
2024-04-03 13:33:10 -0700

    streams

--- node/filesystem/streams.js
+++ node/filesystem/streams.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-const readStream = fs.createReadStream('./Blog/blog3.txt')
-const writeStream = fs.createWriteStream('./Blog/blog5.txt')
+const readStream = fs.createReadStream('dataset.txt')
+const writeStream = fs.createWriteStream('dataset1.txt')
 readStream.on('data', (chunk) => {
     console.log('   NEW CHUNK   ')
     console.log(chunk.toString())
