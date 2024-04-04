ea35d6413a16b0ccc2b8a3a280cc210f2bd811f7
Code-Gale <codewhiz001@gmail.com>
2024-04-04 07:04:48 -0700

    server response serving html file

--- node/servers/response_server.js
+++ node/servers/response_server.js
@@ -1,16 +1,21 @@
 const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
 
-const server = http.createServer((request, response) => {
-    console.log(request.url, request.method)
+    res.setHeader('Content-Type', 'text/html')
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err){
+            console.error('Could not read file', err)
+            res.end()
+        }else {
+            res.write(data)
+            res.end()
+        }
+    })
 
-    response.setHeader('Content-Type', 'text/plain')
-
-    response.write('Hello, world')
-    response.end()
 })
 
-
-
-server.listen(3000, 'localhost', () => {    
-    console.log('Listening for requests')
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for requests.')
 })
