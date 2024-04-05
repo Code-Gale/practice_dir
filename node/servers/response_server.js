16db8ae660577a4272253cae85a4fc155f8edbd3
Code-Gale <codewhiz001@gmail.com>
2024-04-05 13:32:23 -0700

    response server

--- node/servers/response_server.js
+++ node/servers/response_server.js
@@ -1,19 +1,32 @@
 const http = require('http')
 const fs = require('fs')
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
 
     res.setHeader('Content-Type', 'text/html')
-    fs.readFile('./views/index.html', (err, data) => {
+
+    let path = './views/';
+    switch (req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+    fs.readFile(path, (err, data) => {
         if (err){
-            console.error('Could not read file', err)
+            console.error('Could not read file. ', err)
             res.end()
-        }else {
+        } else {
             res.write(data)
             res.end()
         }
     })
-
 })
 
 server.listen(3000, 'localhost', () => {
