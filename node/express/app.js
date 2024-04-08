758ed43dd9f108737da46e39c38a843774f4997f
Code-Gale <codewhiz001@gmail.com>
2024-04-08 09:49:21 -0700

    creating app with express, using ejs and then parsing dynamic data

--- node/express/app.js
+++ node/express/app.js
@@ -1,21 +1,28 @@
-const express = require('express')
+const express = require('express');
+//creating app
+const app = express();
 
-const app = express()
+//setting up views
+app.set('view engine', 'ejs')
 
-app.listen(3000)
+// listening port
+app.listen(3000);
 
+//rendering views
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root : __dirname})
+    const blogs = [
+        {title : 'Akila and the Bee', snippet : 'In the benigigi, Akila went for spelling bee and did quite well with the help of his coach'},
+        {title : 'The magician of sound', snippet : 'Long ago, a boy with supersensitive hearing was born and he could pick up sound from 10km radius'},
+        {title : 'Lord of the mysteries', snippet : 'A transmigrator work up in a strange body, waking up and feeling confused, he decided to live this new life to the fullest'}
+    ]
+    res.render('index', {title : "Home", blogs})
 })
-
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root : __dirname})
+    res.render('about', { title : 'About'})
 })
-
-app.get('/about-me', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title : 'Create'})
 })
-
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root : __dirname })
+    res.status(404).render('404', { title : '404'})
 })
